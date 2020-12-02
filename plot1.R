a5dfe7e45b5fcfdd26976b68980c76c85b68fd17
yurica24 <yurica24@gmail.com>
2020-12-01 20:35:29 -0600

    Grafica 1

---
+++ plot1.R
@@ -0,0 +1,23 @@
+#Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+#Using the base plotting system, make a plot showing the total PM2.5 emission from 
+#all sources for each of the years 1999, 2002, 2005, and 2008.
+
+library("data.table")
+path <- getwd()
+path
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+# Prevents histogram from printing in scientific notation
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
